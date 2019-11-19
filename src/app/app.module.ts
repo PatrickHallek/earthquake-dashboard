1a4e7cedc6312d16425f328347c08ba76d382c9c
PatrickHallek <patrickhallek1998@gmail.com>
2019-11-19 20:48:00 +0100

    Declarate eqrthquake component in app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EarthquakeTableComponent } from './components/earthquake-table/earthquake-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EarthquakeTableComponent
   ],
   imports: [
     BrowserModule,
