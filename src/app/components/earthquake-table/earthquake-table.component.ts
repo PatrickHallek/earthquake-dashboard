b1d6f5e82415360b9ff945242c02dfbea05c4acb
PatrickHallek <patrickhallek1998@gmail.com>
2019-11-19 23:03:19 +0100

    Add material datatable to table component

--- src/app/components/earthquake-table/earthquake-table.component.ts
+++ src/app/components/earthquake-table/earthquake-table.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiUsgsService, IEarthquakeProperties } from 'src/app/services/api-usgs.service';
 import { Subscription } from 'rxjs';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-earthquake-table',
@@ -9,10 +12,16 @@ import { Subscription } from 'rxjs';
 })
 export class EarthquakeTableComponent implements OnInit {
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  public displayedColumns: string[] = ['mag', 'place', 'time', 'status', 'type'];
+  public dataSource: MatTableDataSource<IEarthquakeProperties>;
   public earthquakeData: Array<IEarthquakeProperties>;
   private earthquakeDataSub: Subscription;
 
-  constructor(private apiUsgsService: ApiUsgsService) { }
+  constructor(private apiUsgsService: ApiUsgsService) {
+  }
 
   ngOnInit() {
     this.apiUsgsService.getEarthquakeDataByDate('2019-11-18', '2019-11-19');
@@ -20,6 +29,20 @@ export class EarthquakeTableComponent implements OnInit {
       .getEarthquakeDataListener()
       .subscribe((earthquakeData: Array<IEarthquakeProperties>) => {
         this.earthquakeData = earthquakeData;
+        this.dataSource = new MatTableDataSource(this.earthquakeData);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       });
   }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  ngOndelete() { }
 }
