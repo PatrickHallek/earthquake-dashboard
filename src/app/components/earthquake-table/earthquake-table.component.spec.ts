3030e9133158fbd0fc98248ab0cfde85e6692c74
PatrickHallek <patrickhallek1998@gmail.com>
2019-11-19 20:47:14 +0100

    Add initial earthquake table component files

---
+++ src/app/components/earthquake-table/earthquake-table.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EarthquakeTableComponent } from './earthquake-table.component';
+
+describe('EarthquakeTableComponent', () => {
+  let component: EarthquakeTableComponent;
+  let fixture: ComponentFixture<EarthquakeTableComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EarthquakeTableComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EarthquakeTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/earthquake-table/earthquake-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-earthquake-table',
+  templateUrl: './earthquake-table.component.html',
+  styleUrls: ['./earthquake-table.component.scss']
+})
+export class EarthquakeTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
