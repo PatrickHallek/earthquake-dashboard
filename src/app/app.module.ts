de239b9afe18f6a346c0259004414f41b4872513
PatrickHallek <patrickhallek1998@gmail.com>
2019-11-19 22:26:34 +0100

    Declarate material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
 
 import { AppComponent } from './app.component';
 import { EarthquakeTableComponent } from './components/earthquake-table/earthquake-table.component';
@@ -18,8 +19,10 @@ import { HomeComponent } from './screens/home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MaterialModule,
     HttpClientModule
   ],
+  entryComponents: [EarthquakeTableComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
