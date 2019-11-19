5f3b3e0a7d4b3bdc48c0c9fe78ae6253ade3fb3c
PatrickHallek <patrickhallek1998@gmail.com>
2019-11-19 22:07:52 +0100

    Add usgs api get function and earthquake data interfaces

--- src/app/services/api-usgs.service.ts
+++ src/app/services/api-usgs.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+export interface IEarthquakeProperties {
+  mag: number;
+  place: string;
+  time: number;
+  alert: string;
+  status: string;
+  type: string;
+  // TODO: Add other api parameter if needed
+}
+
+export interface IEarthquakeFeature {
+  properties: IEarthquakeProperties;
+  // TODO: Add other api parameter if needed
+}
+
+export interface IEarthquakeData {
+  features: Array<{ properties: IEarthquakeProperties }>;
+  // TODO: Add other api parameter if needed
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiUsgsService {
 
-  constructor() { }
+  private earthquakeData = new Subject<Array<IEarthquakeProperties>>();
+
+  constructor(private http: HttpClient) { }
+
+  public getEarthquakeDataListener() {
+    return this.earthquakeData.asObservable();
+  }
+
+  public getEarthquakeDataByDate(starttime: string, endtime: string) {
+    const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + starttime + '&endtime=' + endtime;
+    this.http.get(url).pipe(
+      map((result: IEarthquakeData) => {
+        return result.features.map((properties: IEarthquakeFeature) => {
+          const earthquakeFeatureMap: IEarthquakeProperties = {
+            mag: properties.properties.mag,
+            place: properties.properties.place,
+            time: properties.properties.time,
+            alert: properties.properties.alert,
+            status: properties.properties.status,
+            type: properties.properties.type,
+          };
+          return earthquakeFeatureMap;
+        });
+      })
+    ).subscribe((result: Array<IEarthquakeProperties>) => {
+      this.earthquakeData.next(result);
+    },
+      err => console.error(err)
+    );
+  }
 }
