56329be23fb8cb58f90cd36e33d5485593f3b4ec
Hallek <Patrick.Hallek@EXXETA.com>
2019-11-20 13:01:54 +0100

    Add significant earthquakes for past day api call to service

--- src/app/services/api-usgs.service.ts
+++ src/app/services/api-usgs.service.ts
@@ -30,12 +30,18 @@ export interface IEarthquakeData {
 export class ApiUsgsService {
 
   private earthquakeData = new Subject<Array<IEarthquakeProperties>>();
+  private significantEarthquakeData = new Subject<Array<IEarthquakeProperties>>();
 
   constructor(private http: HttpClient) { }
 
   public getEarthquakeDataListener() {
     return this.earthquakeData.asObservable();
   }
+
+  public getSignificantEarthquakeDataListener() {
+    return this.significantEarthquakeData.asObservable();
+  }
+
 
   public getEarthquakeDataByDate(starttime: string, endtime: string) {
     const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime='
@@ -57,6 +63,30 @@ export class ApiUsgsService {
       })
     ).subscribe((result: Array<IEarthquakeProperties>) => {
       this.earthquakeData.next(result);
+    },
+      err => console.error(err)
+    );
+  }
+
+  public getSignificantEarthquakePastDay() {
+    const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson';
+    this.http.get(url).pipe(
+      map((result: IEarthquakeData) => {
+        return result.features.map((properties: IEarthquakeFeature) => {
+          const earthquakeFeatureMap: IEarthquakeProperties = {
+            mag: properties.properties.mag,
+            place: properties.properties.place,
+            time: new Date(properties.properties.time).toISOString().slice(0, 19).replace('T', ' '),
+            alert: properties.properties.alert,
+            status: properties.properties.status,
+            type: properties.properties.type,
+            sig: properties.properties.sig
+          };
+          return earthquakeFeatureMap;
+        });
+      })
+    ).subscribe((result: Array<IEarthquakeProperties>) => {
+      this.significantEarthquakeData.next(result);
     },
       err => console.error(err)
     );
