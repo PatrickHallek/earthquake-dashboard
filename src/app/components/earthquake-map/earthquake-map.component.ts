f99a4640921be2ec01f6bf3de0c993fee1f25523
Hallek <Patrick.Hallek@EXXETA.com>
2019-11-22 09:29:15 +0100

    Add earthquake map component

---
+++ src/app/components/earthquake-map/earthquake-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-earthquake-map',
+  templateUrl: './earthquake-map.component.html',
+  styleUrls: ['./earthquake-map.component.scss']
+})
+export class EarthquakeMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
