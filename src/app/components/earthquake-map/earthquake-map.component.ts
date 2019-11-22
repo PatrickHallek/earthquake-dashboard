0b7dca303599e6be1b67d2d7a6bb349d8eb630d2
Hallek <Patrick.Hallek@EXXETA.com>
2019-11-22 10:30:56 +0100

    Add earthquake map data subscription

--- src/app/components/earthquake-map/earthquake-map.component.ts
+++ src/app/components/earthquake-map/earthquake-map.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ApiUsgsService, IEarthquakeFeature } from 'src/app/services/api-usgs.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-earthquake-map',
   templateUrl: './earthquake-map.component.html',
   styleUrls: ['./earthquake-map.component.scss']
 })
-export class EarthquakeMapComponent implements OnInit {
-
-  constructor() { }
+export class EarthquakeMapComponent implements OnInit, OnDestroy {
+  lat = 51.678418;
+  lng = 7.809007;
+  public earthquakeData: Array<IEarthquakeFeature>;
+  private earthquakeDataSub: Subscription;
+  constructor(private apiUsgsService: ApiUsgsService) { }
 
   ngOnInit() {
+    this.earthquakeDataSub = this.apiUsgsService
+      .getEarthquakeDataListener()
+      .subscribe((earthquakeData: Array<IEarthquakeFeature>) => {
+        this.earthquakeData = earthquakeData;
+      });
   }
 
+  ngOnDestroy() {
+    this.earthquakeDataSub.unsubscribe();
+  }
 }
