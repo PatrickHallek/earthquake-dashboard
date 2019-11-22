d253b2c269c759ce145cf8f46935eec0cc2cd1cf
Hallek <Patrick.Hallek@EXXETA.com>
2019-11-22 09:29:58 +0100

    Add google maps api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AgmCoreModule } from '@agm/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,13 +11,17 @@ import { AppComponent } from './app.component';
 import { EarthquakeTableComponent } from './components/earthquake-table/earthquake-table.component';
 import { HomeComponent } from './screens/home/home.component';
 import { EarthquakeNewsComponent } from './components/earthquake-news/earthquake-news.component';
+import { EarthquakeMapComponent } from './components/earthquake-map/earthquake-map.component';
+
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
     EarthquakeTableComponent,
     HomeComponent,
-    EarthquakeNewsComponent
+    EarthquakeNewsComponent,
+    EarthquakeMapComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +30,10 @@ import { EarthquakeNewsComponent } from './components/earthquake-news/earthquake
     MaterialModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.API_KEY
+    })
   ],
   entryComponents: [EarthquakeTableComponent],
   providers: [],
